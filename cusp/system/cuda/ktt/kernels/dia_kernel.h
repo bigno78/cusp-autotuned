1dd0488a6038c6535ec4fc12ffdc5369a77c51ed
Miroslav Demek <xdemek@efi.muni.cz>
2023-03-21 13:49:28 +0100

    Remove old unused kernels

--- cusp/system/cuda/ktt/kernels/dia_kernel.h
+++ cusp/system/cuda/ktt/kernels/dia_kernel.h
@@ -104,41 +104,6 @@ struct UnrolledLoop<IndexType, ValueType1, ValueType2, 0>
 };
 
 
-template <typename IndexType,
-          typename ValueType1,
-          typename ValueType2,
-          typename ValueType3>
-__device__ void
-naive_dia_kernel(const int num_rows,
-                 const int num_cols,
-                 const int num_diagonals,
-                 const int pitch,
-                 const IndexType* diagonal_offsets,
-                 const ValueType1* values,
-                 const ValueType2* x,
-                 ValueType3* y)
-{
-    const IndexType thread_id = BLOCK_SIZE * blockIdx.x + threadIdx.x;
-
-    if (thread_id < num_rows)
-    {
-        ValueType3 sum = ValueType3(0);
-        for (IndexType i = 0; i < num_diagonals; ++i)
-        {
-            IndexType col = diagonal_offsets[i] + thread_id;
-            if (col >= 0 && col < num_cols)
-            {
-                auto diag_val = load_diag_val(&values[i*pitch + thread_id]);
-                auto x_val = load_x_val(&x[col]);
-
-                sum += diag_val * x_val;
-            }
-        }
-        y[thread_id] = sum;
-    }
-}
-
-
 #define PREFETCH_LOAD_ITERATION(iter)                                          \
     IndexType col##iter       = offsets[i + iter - 1] + thread_id;             \
     ValueType1 diag_val##iter = 0;                                             \
@@ -266,6 +231,7 @@ blocked_offsets_dia_kernel(const int num_rows,
     if (thread_id < num_rows)
         y[thread_id] = sum;
 }
+
 
 template <typename IndexType,
           typename ValueType1,
@@ -433,210 +399,8 @@ template <typename IndexType,
           typename ValueType1,
           typename ValueType2,
           typename ValueType3>
-__global__ void
-timed_dia_kernel(const int num_rows,
-                           const int num_cols,
-                           const int num_diagonals,
-                           const int pitch,
-                           const IndexType* diagonal_offsets,
-                           const ValueType1* values,
-                           const ValueType2* x,
-                           ValueType3* y,
-                           long long int* times,
-                           const int times_pitch)
-{
-    const IndexType thread_id = BLOCK_SIZE * blockIdx.x + threadIdx.x;
-    const IndexType row = thread_id;
-
-    __shared__ IndexType offsets[BLOCK_SIZE];
-    ValueType3 sum = ValueType3(0);
-
-    for (int offset_base = 0; offset_base < num_diagonals; offset_base += BLOCK_SIZE)
-    {
-        if (offset_base + threadIdx.x < num_diagonals)
-        {
-            offsets[threadIdx.x] = diagonal_offsets[offset_base + threadIdx.x];
-        }
-
-        __syncthreads();
-
-        int batch_size = BLOCK_SIZE > num_diagonals - offset_base ? num_diagonals - offset_base : BLOCK_SIZE;
-
-        if (row < num_rows)
-        {
-            for (IndexType i = 0; i < batch_size; ++i)
-            {
-                IndexType col = offsets[i] + row;
-                if (col >= 0 && col < num_cols)
-                {
-                    auto diag_val = load_diag_val(&values[ (offset_base + i)*pitch + row ]);
-                    auto to_load = &x[col];
-
-                    long long int start = clock64();
-                    auto x_val = load_x_val(to_load);
-                    long long int end = clock64();
-
-                    if (threadIdx.x % 32 == 0)
-                    {
-                        IndexType k = row / 32;
-                        __stwt(times + (offset_base + i)*times_pitch + k, end - start + 1);
-                    }
-
-                    sum += diag_val*x_val;
-                }
-            }
-        }
-
-        __syncthreads();
-    }
-
-    if (row < num_rows)
-        y[row] = sum;
-}
-
-
-
-template <typename IndexType,
-          typename ValueType1,
-          typename ValueType2,
-          typename ValueType3>
 __device__ void
-cached_x_dia_kernel(const int num_rows,
-                    const int num_cols,
-                    const int num_diagonals,
-                    const int pitch,
-                    const IndexType* diagonal_offsets,
-                    const ValueType1* values,
-                    const ValueType2* x,
-                    ValueType3* y)
-{
-    using ValueType = ValueType3;
-
-    const IndexType thread_id = BLOCK_SIZE * blockIdx.x + threadIdx.x;
-
-    __shared__ IndexType offsets[BLOCK_SIZE];
-    __shared__ ValueType x_cache[BLOCK_SIZE];
-    IndexType cache_start = -IndexType(BLOCK_SIZE);
-    IndexType cache_end = 0;
-
-    IndexType first_row = BLOCK_SIZE * blockIdx.x;
-    IndexType end_row = min(first_row + BLOCK_SIZE, num_rows);
-
-    ValueType sum = ValueType(0);
-
-    for (int offset_base = 0; offset_base < num_diagonals; offset_base += BLOCK_SIZE) {
-        if (offset_base + threadIdx.x < num_diagonals) {
-            offsets[threadIdx.x] = diagonal_offsets[offset_base + threadIdx.x];
-        }
-
-        __syncthreads();
-
-        int batch_size = BLOCK_SIZE > num_diagonals - offset_base ? num_diagonals - offset_base : BLOCK_SIZE;
-
-        if (thread_id < num_rows)
-        {
-            for (IndexType i = 0; i < batch_size; ++i)
-            {
-                IndexType col = offsets[i] + thread_id;
-                ValueType x_val = -1;
-
-                if (col >= 0 && col < num_cols)
-                {
-                    x_val = col < cache_end ? x_cache[col - cache_start] : x[col];
-                    ValueType diag_val = values[(offset_base + i)*pitch + thread_id];
-                    sum += x_val * diag_val;
-                }
-
-                __syncthreads();
-
-                x_cache[threadIdx.x] = x_val;
-                cache_start = first_row + offsets[i];
-                cache_end = end_row + offsets[i];
-
-                __syncthreads();
-            }
-        }
-
-        __syncthreads();
-    }
-
-    if (thread_id < num_rows) {
-        y[thread_id] = sum;
-    }
-}
-
-template <typename IndexType,
-          typename ValueType1,
-          typename ValueType2,
-          typename ValueType3>
-__device__ void
-experimental_dia_kernel(const int num_rows,
-                    const int num_cols,
-                    const int num_diagonals,
-                    const int pitch,
-                    const IndexType* diagonal_offsets,
-                    const ValueType1* values,
-                    const ValueType2* x,
-                    ValueType3* y)
-{
-    using ValueType = ValueType3;
-
-    const IndexType thread_id = BLOCK_SIZE * blockIdx.x + threadIdx.x;
-
-    __shared__ IndexType offsets[BLOCK_SIZE];
-    __shared__ ValueType x_cache[2*BLOCK_SIZE];
-
-    IndexType first_row = BLOCK_SIZE * blockIdx.x;
-    IndexType first_col = offsets[0] + first_row;
-
-    if (0 <= first_col + threadIdx.x && first_col + threadIdx.x < num_cols) {
-        x_cache[threadIdx.x] = x[first_col + threadIdx.x];
-    }
-
-    if (0 <= first_col + BLOCK_SIZE + threadIdx.x && first_col + BLOCK_SIZE + threadIdx.x < num_cols) {
-        x_cache[BLOCK_SIZE + threadIdx.x] = x[first_col + BLOCK_SIZE + threadIdx.x];
-    }
-
-    ValueType sum = ValueType(0);
-
-    for (int offset_base = 0; offset_base < num_diagonals; offset_base += BLOCK_SIZE) {
-        if (offset_base + threadIdx.x < num_diagonals) {
-            offsets[threadIdx.x] = diagonal_offsets[offset_base + threadIdx.x];
-        }
-
-        __syncthreads();
-
-        int batch_size = BLOCK_SIZE > num_diagonals - offset_base ? num_diagonals - offset_base : BLOCK_SIZE;
-
-        if (thread_id < num_rows)
-        {
-            for (IndexType i = 0; i < batch_size; ++i)
-            {
-                IndexType col = offsets[i] + thread_id;
-
-                if (col >= 0 && col < num_cols)
-                {
-                    ValueType x_val = x_cache[col - first_col];
-                    ValueType diag_val = values[(offset_base + i)*pitch + thread_id];
-                    sum += x_val * diag_val;
-                }
-            }
-        }
-
-        __syncthreads();
-    }
-
-    if (thread_id < num_rows) {
-        y[thread_id] = sum;
-    }
-}
-
-template <typename IndexType,
-          typename ValueType1,
-          typename ValueType2,
-          typename ValueType3>
-__device__ void
-cached_x_naive_dia_kernel(const int num_rows,
+naive_dia_kernel(const int num_rows,
                  const int num_cols,
                  const int num_diagonals,
                  const int pitch,
@@ -645,31 +409,19 @@ cached_x_naive_dia_kernel(const int num_rows,
                  const ValueType2* x,
                  ValueType3* y)
 {
-    using ValueType = ValueType3;
-
     const IndexType thread_id = BLOCK_SIZE * blockIdx.x + threadIdx.x;
 
-    __shared__ ValueType x_cache[2*BLOCK_SIZE];
-
-    IndexType first_row = BLOCK_SIZE * blockIdx.x;
-    IndexType first_col = diagonal_offsets[0] + first_row;
-
-    if (0 <= first_col + threadIdx.x && first_col + threadIdx.x < num_cols) {
-        x_cache[threadIdx.x] = x[first_col + threadIdx.x];
-    }
-
-    if (0 <= first_col + BLOCK_SIZE + threadIdx.x && first_col + BLOCK_SIZE + threadIdx.x < num_cols) {
-        x_cache[BLOCK_SIZE + threadIdx.x] = x[first_col + BLOCK_SIZE + threadIdx.x];
-    }
-
-
-    if (thread_id < num_rows) {
-        ValueType sum = ValueType(0);
-        for (IndexType i = 0; i < num_diagonals; ++i) {
+    if (thread_id < num_rows)
+    {
+        ValueType3 sum = ValueType3(0);
+        for (IndexType i = 0; i < num_diagonals; ++i)
+        {
             IndexType col = diagonal_offsets[i] + thread_id;
-            if (col >= 0 && col < num_cols) {
-                auto diag_val = values[i*pitch + thread_id];
-                auto x_val = x_cache[col - first_col];
+            if (col >= 0 && col < num_cols)
+            {
+                auto diag_val = load_diag_val(&values[i*pitch + thread_id]);
+                auto x_val = load_x_val(&x[col]);
+
                 sum += diag_val * x_val;
             }
         }
@@ -677,20 +429,20 @@ cached_x_naive_dia_kernel(const int num_rows,
     }
 }
 
+
 template <typename IndexType,
           typename ValueType1,
           typename ValueType2,
           typename ValueType3>
-__launch_bounds__(BLOCK_SIZE,1) __global__ void
-ktt_dia_vector_kernel(
-                const int num_rows,
-                const int num_cols,
-                const int num_diagonals,
-                const int pitch,
-                const IndexType* diagonal_offsets,
-                const ValueType1* values,
-                const ValueType2* x,
-                ValueType3* y)
+__launch_bounds__(BLOCK_SIZE, 1) __global__ void
+ktt_dia_vector_kernel(const int num_rows,
+                      const int num_cols,
+                      const int num_diagonals,
+                      const int pitch,
+                      const IndexType* diagonal_offsets,
+                      const ValueType1* values,
+                      const ValueType2* x,
+                      ValueType3* y)
 {
 #if KERNEL_TYPE == 0
     naive_dia_kernel<IndexType, ValueType1, ValueType2, ValueType3>
