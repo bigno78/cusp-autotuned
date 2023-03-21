e48d543159ca998eb3d3b32eb97c172cce496444
Miroslav Demek <xdemek@efi.muni.cz>
2023-03-21 09:24:05 +0100

    Fix formatting

--- cusp/system/cuda/ktt/dia_multiply.h
+++ cusp/system/cuda/ktt/dia_multiply.h
@@ -6,12 +6,9 @@
 #include <cusp/system/cuda/ktt/utils.h>
 #include <cusp/ktt/detail/external/nameof.hpp>
 
-#include <cusp/system/cuda/arch.h> // max_active_blocks
 #include <cusp/system/cuda/utils.h>
 #include <cusp/dia_matrix.h>
 
-#include <optional>
-#include <iostream>
 
 namespace cusp {
 
@@ -23,23 +20,28 @@ namespace ktt {
 
 namespace dia {
 
-inline void setup_tuning_parameters(::ktt::Tuner& tuner, const kernel_context& kernel)
-{
-    tuner.AddParameter(kernel.kernel_id, "KERNEL_TYPE", std::vector<uint64_t>{ 0, 1, 2 });
-    tuner.AddParameter(kernel.kernel_id, "SHARED_PREFETCH_FACTOR", std::vector<uint64_t>{ 0, 2, 4, 8 });
-    tuner.AddParameter(kernel.kernel_id, "REGISTER_PREFETCH_FACTOR", std::vector<uint64_t>{ 0, 2, 3, 4 });
-    tuner.AddParameter(kernel.kernel_id, "REGISTER_PREFETCH_TYPE", std::vector<uint64_t>{ 0, 1 });
-    tuner.AddParameter(kernel.kernel_id, "LOAD_TYPE", std::vector<uint64_t>{ 0, 1 });
-    tuner.AddParameter(kernel.kernel_id, "STRIPING_FACTOR", std::vector<uint64_t>{ 2, 4, 8 });
-    tuner.AddParameter(kernel.kernel_id, "BLOCK_SIZE", std::vector<uint64_t>{ 256, 512 });
 
-    // START OF HACKS ---------------------
-    tuner.AddParameter(kernel.kernel_id, "BLOCKS_PER_SECTOR", std::vector<uint64_t>{ 0 });
-    tuner.AddParameter(kernel.kernel_id, "CHUNK_SIZE", std::vector<uint64_t>{ 0 });
-    tuner.AddParameter(kernel.kernel_id, "STRIPE_SIZE", std::vector<uint64_t>{ 0 });
-    tuner.AddParameter(kernel.kernel_id, "GRID_SIZE", std::vector<uint64_t>{ 0 });
-    tuner.AddParameter(kernel.kernel_id, "SECTOR_MAPPING_TYPE",
-                       std::vector<uint64_t>{ 0, 1 });
+inline void setup_tuning_parameters(const kernel_context& kernel)
+{
+    auto& tuner = *kernel.tuner;
+    auto kernel_id = kernel.kernel_id;
+
+    tuner.AddParameter(kernel_id, "KERNEL_TYPE", u64_vec{ 0, 1, 2 });
+    tuner.AddParameter(kernel_id, "SHARED_PREFETCH_FACTOR",
+                       u64_vec{ 0, 2, 4, 8 });
+    tuner.AddParameter(kernel_id, "REGISTER_PREFETCH_FACTOR",
+                       u64_vec{ 0, 2, 3, 4 });
+    tuner.AddParameter(kernel_id, "REGISTER_PREFETCH_TYPE", u64_vec{ 0, 1 });
+    tuner.AddParameter(kernel_id, "LOAD_TYPE", u64_vec{ 0, 1 });
+    tuner.AddParameter(kernel_id, "STRIPING_FACTOR", u64_vec{ 2, 4, 8 });
+    tuner.AddParameter(kernel_id, "BLOCK_SIZE", u64_vec{ 256, 512 });
+
+    // START OF HACKS to test striping ----
+    tuner.AddParameter(kernel_id, "BLOCKS_PER_SECTOR", u64_vec{ 0 });
+    tuner.AddParameter(kernel_id, "CHUNK_SIZE", u64_vec{ 0 });
+    tuner.AddParameter(kernel_id, "STRIPE_SIZE", u64_vec{ 0 });
+    tuner.AddParameter(kernel_id, "GRID_SIZE", u64_vec{ 0 });
+    tuner.AddParameter(kernel_id, "SECTOR_MAPPING_TYPE", u64_vec{ 0, 1 });
 
     tuner.AddConstraint(kernel.kernel_id, { "SECTOR_MAPPING_TYPE" },
                         [] (const std::vector<uint64_t>& values) {
@@ -53,46 +55,55 @@ inline void setup_tuning_parameters(::ktt::Tuner& tuner, const kernel_context& k
         ::ktt::ModifierType::Local,
         ::ktt::ModifierDimension::X,
         { std::string("BLOCK_SIZE") },
-        [] (const uint64_t defaultSize, const std::vector<uint64_t>& parameters) {
+        [] (const uint64_t defaultSize, const u64_vec& parameters) {
             return parameters[0];
-        }
-    );
+        });
 
-    // only one type of prefetching can be applied at once
-    tuner.AddConstraint(kernel.kernel_id,
-                        { "SHARED_PREFETCH_FACTOR", "REGISTER_PREFETCH_FACTOR" },
-                        [] (const std::vector<uint64_t>& values) {
-                            return values[0] == 0 || values[1] == 0;
-                        });
+    // Only one type of prefetching can be applied at once.
+    tuner.AddConstraint(
+        kernel.kernel_id,
+        { "SHARED_PREFETCH_FACTOR", "REGISTER_PREFETCH_FACTOR" },
+        [] (const std::vector<uint64_t>& values) {
+            return values[0] == 0 || values[1] == 0;
+        });
 
-    // prefetching is used only for the blocked offsets kernel
-    tuner.AddConstraint(kernel.kernel_id,
-                        { "KERNEL_TYPE", "SHARED_PREFETCH_FACTOR", "REGISTER_PREFETCH_FACTOR" },
-                        [] (const std::vector<uint64_t>& values) {
-                            return values[0] == 1 || (values[1] == 0 && values[2] == 0);
-                        });
+    // Prefetching is used only for the blocked offsets kernel.
+    tuner.AddConstraint(
+        kernel.kernel_id,
+        { "KERNEL_TYPE", "SHARED_PREFETCH_FACTOR", "REGISTER_PREFETCH_FACTOR" },
+        [] (const std::vector<uint64_t>& values) {
+            return values[0] == 1 || (values[1] == 0 && values[2] == 0);
+        });
 
-    // different register prefetching implementations are used only when register prefetching is enabled
-    tuner.AddConstraint(kernel.kernel_id,
-                        { "REGISTER_PREFETCH_FACTOR", "REGISTER_PREFETCH_TYPE" },
-                        [] (const std::vector<uint64_t>& values) {
-                            return values[0] > 0 || values[1] == 0;
-                        });
+    // Different register prefetching implementations are used
+    // only when register prefetching is actually enabled
+    tuner.AddConstraint(
+        kernel.kernel_id,
+        { "REGISTER_PREFETCH_FACTOR", "REGISTER_PREFETCH_TYPE" },
+        [] (const std::vector<uint64_t>& values) {
+            return values[0] > 0 || values[1] == 0;
+        });
 
-    // don't try different striping factors for non-striped kernels
-    tuner.AddConstraint(kernel.kernel_id,
-                    { "KERNEL_TYPE", "STRIPING_FACTOR" },
-                    [] (const std::vector<uint64_t>& values) {
-                        return values[0] == 2 || values[1] == 2;
-                    });
+    // Don't try different striping factors for non-striped kernels.
+    tuner.AddConstraint(
+        kernel.kernel_id,
+        { "KERNEL_TYPE", "STRIPING_FACTOR" },
+        [] (const std::vector<uint64_t>& values) {
+            return values[0] == 2 || values[1] == 2;
+        });
 }
 
-template<typename IndexType, typename ValueType1, typename ValueType2, typename ValueType3>
+
+template<typename IndexType,
+         typename ValueType1,
+         typename ValueType2,
+         typename ValueType3>
 kernel_context initialize_kernel(::ktt::Tuner& tuner)
 {
     kernel_context kernel(tuner);
 
-    std::string kernel_path = STRING(CUSP_PATH) "/cusp/system/cuda/ktt/kernels/dia_kernel.h";
+    std::string kernel_path =
+        STRING(CUSP_PATH) "/cusp/system/cuda/ktt/kernels/dia_kernel.h";
 
     std::vector< std::string > type_names {
         std::string(NAMEOF_TYPE(IndexType)),
@@ -101,6 +112,9 @@ kernel_context initialize_kernel(::ktt::Tuner& tuner)
         std::string(NAMEOF_TYPE(ValueType3))
     };
 
+    // NOTE: These can be anything since they are awlays set in the launcher.
+    // So use some values that will hopefully cause a crash if not set properly
+    // in the launcher.
     const ::ktt::DimensionVector blockDimensions(0);
     const ::ktt::DimensionVector gridDimensions(0);
 
@@ -114,7 +128,7 @@ kernel_context initialize_kernel(::ktt::Tuner& tuner)
     kernel.definition_ids.push_back(definition_id);
     kernel.kernel_id = tuner.CreateSimpleKernel("DiaKernel", definition_id);
 
-    dia::setup_tuning_parameters(tuner, kernel);
+    dia::setup_tuning_parameters(kernel);
 
     return kernel;
 }
@@ -132,7 +146,10 @@ const kernel_context& get_kernel(
         ::ktt::Tuner& tuner,
         const cusp::dia_matrix<IndexType, ValueType1, MemorySpace>& A)
 {
-    static kernel_context kernel = dia::initialize_kernel<IndexType, ValueType1, ValueType2, ValueType3>(tuner);
+    constexpr auto init_kernel =
+        dia::initialize_kernel<IndexType, ValueType1, ValueType2, ValueType3>;
+
+    static kernel_context kernel = init_kernel(tuner);
     return kernel;
 }
 
@@ -141,24 +158,30 @@ template <typename IndexType,
           typename ValueType2,
           typename ValueType3>
 std::vector<::ktt::ArgumentId> add_arguments(
-                                    const kernel_context& kernel,
-                                    const cusp::dia_matrix<IndexType, ValueType1, cusp::device_memory>& A,
-                                    const cusp::array1d<ValueType2, cusp::device_memory>& x,
-                                    cusp::array1d<ValueType3, cusp::device_memory>& y)
+        const kernel_context& kernel,
+        const cusp::dia_matrix<IndexType, ValueType1, cusp::device_memory>& A,
+        const cusp::array1d<ValueType2, cusp::device_memory>& x,
+        cusp::array1d<ValueType3, cusp::device_memory>& y)
 {
     const IndexType num_diagonals = A.values.num_cols;
-    const IndexType pitch = A.values.pitch; // length of one diagonal + possible padding
+    const IndexType pitch = A.values.pitch;
 
-    auto args = add_arguments(*kernel.tuner, A.num_rows, A.num_cols, num_diagonals, pitch, A.diagonal_offsets, A.values.values, x, y);
+    auto args =
+        add_arguments(*kernel.tuner, A.num_rows, A.num_cols, num_diagonals,
+                      pitch, A.diagonal_offsets, A.values.values, x, y);
+
     kernel.tuner->SetArguments(kernel.definition_ids[0], args);
 
     return args;
 }
 
 
-// Returns the argument id of the y vector given a vector of arguments returned by a previous
-// call of `add_arguments`.
-inline ::ktt::ArgumentId get_output_argument(const std::vector<::ktt::ArgumentId>& arguments, cusp::dia_format) {
+// Returns the argument id of the y vector given a vector of arguments
+// returned by a previous call of `add_arguments`.
+inline ::ktt::ArgumentId get_output_argument(
+        const std::vector<::ktt::ArgumentId>& arguments,
+        cusp::dia_format)
+{
     return arguments[7];
 }
 
@@ -176,18 +199,23 @@ auto get_launcher(
 {
     return [&, profile] (::ktt::ComputeInterface& interface)
     {
-        ::ktt::DimensionVector block_size = interface.GetCurrentLocalSize(ctx.definition_ids[0]);
-        ::ktt::DimensionVector grid_size( DIVIDE_INTO(A.num_rows, block_size.GetSizeX()) );
+        ::ktt::DimensionVector block_size =
+            interface.GetCurrentLocalSize(ctx.definition_ids[0]);
+        ::ktt::DimensionVector grid_size(
+            DIVIDE_INTO(A.num_rows, block_size.GetSizeX()));
 
+        // START OF HACKS to test striping ----
         auto conf = interface.GetCurrentConfiguration();
         for (const auto& pair : conf.GetPairs())
             if (pair.GetName() == "GRID_SIZE" && pair.GetValue() != 0)
                 grid_size = ::ktt::DimensionVector(pair.GetValue());
+        // END OF HACKS -----------------------
 
         if (!profile) {
             interface.RunKernel(ctx.definition_ids[0], grid_size, block_size);
         } else {
-            interface.RunKernelWithProfiling(ctx.definition_ids[0], grid_size, block_size);
+            interface.RunKernelWithProfiling(ctx.definition_ids[0],
+                                             grid_size, block_size);
         }
     };
 }
