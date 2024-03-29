06341dfaf7070b3d1b50c3f55c82e859da03c2b1
František Bráblík <frantisek.brablik@gmail.com>
2024-03-29 04:03:58 +0100

    CSR: Finish original cusp warp-oriented implementation

--- cusp/system/cuda/ktt/kernels/csr_kernel.h
+++ cusp/system/cuda/ktt/kernels/csr_kernel.h
@@ -8,13 +8,17 @@ void csr_kernel_naive(const unsigned int num_rows,
                 const Val2*  x,
                 Val3*        y)
 {
-    const int idx = BLOCK_SIZE * blockIdx.x + threadIdx.x;
+    // const int idx = BLOCK_SIZE * blockIdx.x + threadIdx.x;
+    int idx = BLOCK_SIZE * blockIdx.x + threadIdx.x;
+    const int total = BLOCK_SIZE * gridDim.x;
 
     Val1 value = 0;
-    if (idx < num_rows)
+    // if (idx < num_rows)
+    for (; idx < num_rows; idx += total)
     {
         Idx row_start = Ar[idx];
         Idx row_end = Ar[idx + 1];
+        // TODO: s/int/Idx/
         for (int i = row_start; i < row_end; ++i)
         {
             // Val3 value = Ax[i] * x[Ac[i]];
@@ -121,74 +125,62 @@ void csr_kernel_warp(const unsigned int num_rows,
                 const Val2*  x,
                 Val3*        y)
 {
-    const int WARP_SIZE = 32;
-
-    const int lane = threadIdx.x % WARP_SIZE;
+    const int ti = BLOCK_SIZE * blockIdx.x + threadIdx.x;
+    const int lane = threadIdx.x % THREADS_PER_ROW;
     const int idx_in_blk = threadIdx.x;
     const int blk_idx = blockIdx.x;
 
-    const int row = blk_idx;
-
-    // const int ti = BLOCK_SIZE * blockIdx.x + idx_in_blk;
-
-    // __shared__ Val1 sh_sum;
-
-    // if (idx_in_blk == 0)
-    //     sh_sum = 0;
+    const int vectors_per_block = BLOCK_SIZE / THREADS_PER_ROW;
+    const int vector_count = gridDim.x * vectors_per_block;
 
     __shared__ Idx sh_row_info[2];
 
-    if (blk_idx >= num_rows)
-        return;
+    for (Idx row = ti / THREADS_PER_ROW; row < num_rows; row += vector_count)
+    {
 
     // TODO: fetch using two threads like cusp does
-    // Idx row_start = Ar[ blk_idx ];
-    // Idx row_end   = Ar[ blk_idx + 1 ];
-    if (idx_in_blk < 2)
-        sh_row_info[ idx_in_blk ] = Ar[ row + idx_in_blk ];
-    __syncthreads();
-    Idx row_start = sh_row_info[ 0 ];
-    Idx row_end   = sh_row_info[ 1 ];
+    Idx row_start = Ar[ row ];
+    Idx row_end   = Ar[ row + 1 ];
+    // if (idx_in_blk < 2)
+    //     sh_row_info[ idx_in_blk ] = Ar[ row + idx_in_blk ];
+    // __syncthreads();
+    // Idx row_start = sh_row_info[ 0 ];
+    // Idx row_end   = sh_row_info[ 1 ];
 
 
     Val3 value = 0;
 
-    if (row_end - row_start > 32)
+    if (THREADS_PER_ROW == 32 && row_end - row_start > 32)
     {
         Idx aligned_start = row_start - exp2mod(row_start, 32) + lane;
 
         if (int i = aligned_start; i >= row_start && i < row_end)
             value += Ax[ i ] * x[ Ac[ i ] ];
 
-        for (int i = aligned_start + BLOCK_SIZE; i < row_end; i += BLOCK_SIZE)
+        for (int i = aligned_start + THREADS_PER_ROW; i < row_end; i += THREADS_PER_ROW)
             value += Ax[ i ] * x[ Ac[ i ] ];
     }
     else
     {
-        for (int i = row_start + lane; i < row_end; i += BLOCK_SIZE)
+        for (int i = row_start + lane; i < row_end; i += THREADS_PER_ROW)
             value += Ax[ i ] * x[ Ac[ i ] ];
     }
 
 
-    const unsigned mask = 0xffffffff;
-    value += __shfl_down_sync(mask, value, 16);
-    value += __shfl_down_sync(mask, value,  8);
-    value += __shfl_down_sync(mask, value,  4);
-    value += __shfl_down_sync(mask, value,  2);
-    value += __shfl_down_sync(mask, value,  1);
+    constexpr unsigned mask = 0xffffffff;
+    #pragma unroll
+    for (int off = THREADS_PER_ROW / 2; off >= 1; off /= 2)
+        value += __shfl_down_sync(mask, value, off);
 
-    // TODO: later
-    // if (lane == 0)
-    //     sh_vals[ idx_in_blk ] = value;
+    // value += __shfl_down_sync(mask, value, 16);
+    // value += __shfl_down_sync(mask, value,  8);
+    // value += __shfl_down_sync(mask, value,  4);
+    // value += __shfl_down_sync(mask, value,  2);
+    // value += __shfl_down_sync(mask, value,  1);
 
-    // TODO: solve reduction for block size > 32
-    if (idx_in_blk == 0)
-    {
-        // Val1 sum_warps = 0;
-        // for (int j = 0; j < BLOCK_SIZE; j += WARP_SIZE)
-        //     sum_warps += sh_vals[ j ];
-        // sh_sum += sum_warps;
-        y[ blk_idx ] = value;
+    if (lane == 0)
+        y[ row ] = value;
+
     }
 }
 
@@ -215,25 +207,35 @@ void csr_kernel_block(const unsigned int num_rows,
     const int idx_in_blk = threadIdx.x;
     const int warp_in_block = idx_in_blk / WARP_SIZE;
 
-    const int row = blk_idx;
+    const int BLOCK_COUNT = gridDim.x;
+
+    // const int row = blk_idx;
+
+    // if (blk_idx >= num_rows)
+    //     return;
 
     __shared__ Val3 sh_sums[ BLOCK_SIZE / WARP_SIZE ];
     __shared__ Idx sh_row_info[2];
 
+    // TODO: better fetching of row info
+    // for (int row = blk_idx * ROWS_PER_BLOCK; row < blk_idx * ROWS_PER_BLOCK + ROWS_PER_BLOCK; ++row)
+    int begin = blk_idx;
+    for (unsigned row = begin; row < num_rows; row += BLOCK_COUNT)
+    {
     // if (lane == 0)
     //     sh_sums[ warp_in_block ] = 0;
 
-    if (blk_idx >= num_rows)
-        return;
 
     // TODO: fetch using two threads like cusp does
-    // Idx row_start = Ar[ blk_idx ];
-    // Idx row_end = Ar[ blk_idx + 1 ];
-    if (idx_in_blk < 2)
-        sh_row_info[ idx_in_blk ] = Ar[ row + idx_in_blk ];
-    __syncthreads();
-    const Idx row_start = sh_row_info[ 0 ];
-    const Idx row_end   = sh_row_info[ 1 ];
+    Idx row_start = Ar[ row ];
+    Idx row_end = Ar[ row + 1 ];
+
+    // if (idx_in_blk < 2)
+    //     sh_row_info[ idx_in_blk ] = Ar[ row + idx_in_blk ];
+    // __syncthreads();
+    // const Idx row_start = sh_row_info[ 0 ];
+    // const Idx row_end   = sh_row_info[ 1 ];
+
 
     Val3 value = 0;
 
@@ -252,30 +254,30 @@ void csr_kernel_block(const unsigned int num_rows,
 
     __syncthreads();
 
-    // if (idx_in_blk == 0)
-    // {
-    //     Val1 total_sum = 0;
-    //     #pragma unroll
-    //     for (int j = 0; j < BLOCK_SIZE / WARP_SIZE; ++j)
-    //         total_sum += sh_sums[ j ];
-    //     y[ row ] = total_sum;
-    // }
-
-    const int WARPS_PER_BLOCK = BLOCK_SIZE / WARP_SIZE;
-
-    if (idx_in_blk < WARPS_PER_BLOCK / 2)
+    if (idx_in_blk == 0)
     {
-        for (int offset = WARPS_PER_BLOCK / 2; offset > 0; offset /= 2)
-        {
-            auto my_val = sh_sums[ warp_in_block ];
-            auto friend_val = sh_sums[ warp_in_block + offset ];
-            sh_sums[ warp_in_block ] = my_val + friend_val;
-            __syncthreads();
-        }
+        Val1 total_sum = 0;
+        #pragma unroll
+        for (int j = 0; j < BLOCK_SIZE / WARP_SIZE; ++j)
+            total_sum += sh_sums[ j ];
+        y[ row ] = total_sum;
     }
 
-    if (idx_in_blk == 0)
-        y[ row ] = sh_sums[ 0 ];
+    // TODO: these is some bug in this that makes it loop forever
+    // const int WARPS_PER_BLOCK = BLOCK_SIZE / WARP_SIZE;
+    // if (idx_in_blk < WARPS_PER_BLOCK / 2)
+    // {
+    //     for (int offset = WARPS_PER_BLOCK / 2; offset > 0; offset /= 2)
+    //     {
+    //         auto my_val = sh_sums[ warp_in_block ];
+    //         auto friend_val = sh_sums[ warp_in_block + offset ];
+    //         sh_sums[ warp_in_block ] = my_val + friend_val;
+    //         __syncthreads();
+    //     }
+    // }
+    // if (idx_in_blk == 0)
+    //     y[ row ] = sh_sums[ 0 ];
+    }
 }
 
 
@@ -289,7 +291,10 @@ void csr_spmv(const unsigned int num_rows,
               const Val2*  x,
               Val3*        y)
 {
-    // csr_kernel_block<Idx, Val1, Val2, Val3>(num_rows, Ar, Ac, Ax, x, y);
-    csr_kernel_warp<Idx, Val1, Val2, Val3>(num_rows, Ar, Ac, Ax, x, y);
-    // csr_kernel_naive<Idx, Val1, Val2, Val3>(num_rows, Ar, Ac, Ax, x, y);
+    if constexpr (THREADS_PER_ROW == 1)
+        csr_kernel_naive<Idx, Val1, Val2, Val3>(num_rows, Ar, Ac, Ax, x, y);
+    else if constexpr (THREADS_PER_ROW <= 32)
+        csr_kernel_warp<Idx, Val1, Val2, Val3>(num_rows, Ar, Ac, Ax, x, y);
+    else
+        csr_kernel_block<Idx, Val1, Val2, Val3>(num_rows, Ar, Ac, Ax, x, y);
 }
